59a443300e0d36c8bcea49920c788cd5efdc6c64
BanbanProduction <79019044+BanbanProduction@users.noreply.github.com>
2022-02-16 12:30:45 +0100

    Initial commit

--- Window.py
+++ Window.py
@@ -5,10 +5,14 @@ class Window(tk.Tk):
 
     def __init__(self, *args, **kwargs):
         super().__init__()
+
+        width, height = 679, 406
         self.title("Gest Money")
-        self.geometry("679x406")
+        self.geometry(f"{width}x{height}")
+        self.minsize(width, height)
+        self.maxsize(width, height)
         # window.iconbitmap("")
         self.config(bg='#A27147')
 
-    def update(self):
+    def run(self):
         self.mainloop()
--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from Window import Window
 window = Window()
 
 if __name__ == '__main__':
-    window.update()
+    window.run()
