bb9a8e8929c15daee239b90526ee80f5ea33104a
Katsuhi-Dev <maxencedu62_@outlook.fr>
2022-03-01 16:26:29 +0100

    Problème json

--- data/data.py
+++ data/data.py
@@ -4,11 +4,13 @@ import json
 def add_user_in_activity_recent(user):
     with open(r'..\..\data\activity.json', 'r+') as file:
         data = json.load(file)
-        data['activity_recent'].insert(0, user)
+        if user not in data['activity_recent']:
+            data['activity_recent'].insert(0, user)
         if len(data['activity_recent']) >= 4:
             x = data['activity_recent']
             x.remove(x[-1])
-        file.seek(0)
+
+    with open(r'..\..\data\activity.json', 'w') as file:
         json.dump(data, file, indent=4)
 
 
