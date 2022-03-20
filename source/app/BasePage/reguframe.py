2022f5d29c37f537ee0017b0a874f82469a9e728
BanbanProduction <79019044+BanbanProduction@users.noreply.github.com>
2022-03-20 19:46:17 +0100

    Enlèvement imports inutiles

--- source/app/BasePage/reguframe.py
+++ source/app/BasePage/reguframe.py
@@ -1,21 +1,24 @@
 import tkinter as tk
 
-from data.data import add_transaction
-from source.app.BasePage.baseframe import create_copyright, create_buttons, show_error, date_valid
+from source.app.BasePage.baseframe import create_copyright, create_buttons, show_error
 from source.app.Sys import set_color
 
 
 class ReguFrame(tk.Frame):
     def __init__(self, window):
         self.window = window
-        super().__init__(window, width=1023, height=640, bg=self.set_color('fourthbg'))
         frame_width = 1023
+
+        super().__init__(window, width=frame_width, height=640, bg=self.set_color('fourthbg'))
 
         self.error_canvas = tk.Canvas()
 
-        self.canvas = tk.Canvas(self, height=640, width=1023, background=self.set_color('fourthbg'),
+        self.canvas = tk.Canvas(self, height=640, width=frame_width, background=self.set_color('fourthbg'),
                                 highlightthickness=0)
-        self.canvas.create_text(frame_width / 4, 50, text="Dépense Régulière", font=('Roboto', 30),
+        self.canvas.create_text(215, 50, text="""
+    Créer une
+dépense régulière
+""", font=('Roboto', 30),
                                 fill=self.set_color('text2'))
 
         create_copyright(self, self.canvas)
@@ -24,57 +27,32 @@ class ReguFrame(tk.Frame):
         self.canvas.pack()
 
     def create_inputs(self):
-        self.canvas.create_text(80, 145, text='Montant', font=('Roboto', 18), fill=self.set_color('text'), anchor='w')
-        self.canvas.create_text(80, 255, text='Origine de la somme', font=('Roboto', 18),
-                                fill=self.set_color('text'), anchor='w')
-        self.canvas.create_text(80, 365, text='Date', font=('Roboto', 18), fill=self.set_color('text'), anchor='w')
+        x_pos = 50
+        entry_width = 330
+        entry_height = 46
 
-        self.canvas.create_text(73, 455, text='Type', font=('Roboto', 18),
+        self.canvas.create_text(x_pos, 145, text='Montant', font=('Roboto', 18),
+                                fill=self.set_color('text'), anchor='w')
+        self.canvas.create_text(x_pos, 255, text='Objet', font=('Roboto', 18),
+                                fill=self.set_color('text'), anchor='w')
+        self.canvas.create_text(x_pos, 365, text='Jour du prélèvement', font=('Roboto', 18),
                                 fill=self.set_color('text'), anchor='w')
 
         self.amount = tk.Entry(self.canvas, bg=self.set_color('bg'), font=('Roboto', 15), fg='white',
                                bd=0, insertbackground=self.set_color('entrytext'))
-        self.amount.place(x=50, y=165, width=330, height=46)
+        self.amount.place(x=x_pos, y=165, width=entry_width, height=entry_height)
 
         self.origin = tk.Entry(self.canvas, bg=self.set_color('bg'), font=('Roboto', 15), fg='white',
                                bd=0, insertbackground=self.set_color('entrytext'))
-        self.origin.place(x=50, y=275, width=330, height=46)
+        self.origin.place(x=x_pos, y=275, width=entry_width, height=entry_height)
 
         self.date = tk.Entry(self.canvas, bg=self.set_color('bg'), font=('Roboto', 15), fg='white',
                              bd=0, insertbackground=self.set_color('entrytext'))
-        self.date.place(x=50, y=275 + 110, width=330, height=46)
+        self.date.place(x=x_pos, y=275 + 110, width=entry_width, height=entry_height)
 
-        cheque_var = tk.IntVar()
-        virement_var = tk.IntVar()
-        especes_var = tk.IntVar()
-        self.check_vars = [virement_var, especes_var, cheque_var]
+        create_buttons(self, self.valid_debit, 30, 240, 160)
 
-        vir_checkbutton = tk.Checkbutton(self, text='Virement', background=self.set_color('fourthbg'),
-                                         foreground=self.set_color('text'), font=('Roboto', 16),
-                                         highlightthickness=0, bd=0, activebackground=self.set_color('fourthbg'),
-                                         activeforeground=self.set_color('text'), variable=virement_var,
-                                         command=lambda: self.uncheck_buttons(0))
-        vir_checkbutton.place(x=43, y=475)
-
-        espece_checkbutton = tk.Checkbutton(self, text='Espèce', background=self.set_color('fourthbg'), bd=0,
-                                            foreground=self.set_color('text'), font=('Roboto', 16),
-                                            activebackground=self.set_color('fourthbg'), highlightthickness=0,
-                                            activeforeground=self.set_color('text'), variable=especes_var,
-                                            command=lambda: self.uncheck_buttons(1))
-        espece_checkbutton.place(x=168, y=475)
-
-        cheque_checkbutton = tk.Checkbutton(self, text='Chèque', background=self.set_color('fourthbg'), bd=0,
-                                            foreground=self.set_color('text'), font=('Roboto', 16),
-                                            activebackground=self.set_color('fourthbg'), highlightthickness=0,
-                                            activeforeground=self.set_color('text'), variable=cheque_var,
-                                            command=lambda: self.uncheck_buttons(2))
-        cheque_checkbutton.place(x=283, y=475)
-
-        self.check_buttons = [vir_checkbutton, espece_checkbutton, cheque_checkbutton]
-
-        create_buttons(self, self.valid_credit)
-
-    def valid_credit(self):
+    def valid_debit(self):
         origin = self.origin.get()
         amount = self.amount.get()
         date = self.date.get()
@@ -92,31 +70,16 @@ class ReguFrame(tk.Frame):
         elif not amount.isdigit():
             self.show_error('Veuillez entrer un montant valide')
 
-        elif not date_valid(date):
-            self.show_error('Veuillez entrer une date au format dd/mm/yy')
-
-        elif 1 not in [var.get() for var in self.check_vars]:
-            self.show_error('Veuillez choisir un mode de paiement')
+        elif not date.isdigit() or int(date) > 31:
+            self.show_error('Veuillez entrer un jour valide')
 
         else:
             transaction['amount'] = int(transaction['amount'])
-            methods_names = ['Virement', 'Espèces', 'Chèque']
-
-            for var in self.check_vars:
-                if var.get() == 1:
-                    transaction['method'] = methods_names[self.check_vars.index(var)]
-
-            add_transaction(self.window.user_id, transaction)
 
             self.window.switch_frame('BasePage')
 
     def show_error(self, text):
-        show_error(self, text)
-
-    def uncheck_buttons(self, exception):
-        for button in self.check_buttons:
-            if button != self.check_buttons[exception]:
-                button.deselect()
+        show_error(self, text, 215)
 
     def set_color(self, color):
         return set_color(self.window.color_theme, color)
